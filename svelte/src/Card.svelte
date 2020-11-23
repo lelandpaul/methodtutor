f49545af2ab68a3006c9ae90a2006d5ea89d8fd8
lelandpaul <lelandpaul@gmail.com>
2020-11-23 14:15:57 -0500

    color card border by result

--- svelte/src/Card.svelte
+++ svelte/src/Card.svelte
@@ -1,13 +1,43 @@
 <script>
+  import { mistakes, card_complete } from './stores.js';
   export let method;
   export let bell;
   export let width;
   export let small = true;
+
+  let done_color;
+  $: switch ($mistakes) {
+    case 0:
+      done_color = 'success';
+      break;
+    case 1:
+    case 2:
+      done_color = 'primary';
+      break;
+    case 3:
+    case 4:
+      done_color = 'warning';
+      break;
+    default:
+      done_color = 'danger';
+      break;
+  }
+
+  $: console.log($card_complete, done_color);
+
 </script>
+
+<style>
+
+
+
+</style>
 
 
 <div class="card mx-auto mb-2"
-     style="width: {width}">
+  style="width: {width}; 
+  border-color: {$card_complete ? 'var(--' + done_color + ')' : 'rgba(0,0,0,0.125)'};
+  border-width: {$card_complete ? '3px' : '1px' };">
   <div class="card-body py-0 text-center">
     {#if small}
       <h6 class="card-title mt-3 mb-3">
